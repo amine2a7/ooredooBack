43e445719d852362518f37bc6c6bbd1e6488e705
amine <mohamedamine.khadraoui1@esprit.tn>
2024-06-21 11:22:46 +0100

    ser

--- backend/server.js
+++ backend/server.js
@@ -58,3 +58,19 @@ require('dotenv').config();
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+
+
+////////////////////
+// const express = require('express');
+// const app = express();
+// const port = process.env.PORT || 3000;
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
+
+// app.listen(port, '0.0.0.0', () => {
+//   console.log(`Server is running on http://0.0.0.0:${port}`);
+// });
+////////////////////////
