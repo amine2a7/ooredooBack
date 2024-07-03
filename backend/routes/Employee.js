87d9230862d226cf576af3d0180f16750f875c67
amine <mohamedamine.khadraoui1@esprit.tn>
2024-07-03 10:26:49 +0100

    rt

--- backend/routes/Employee.js
+++ backend/routes/Employee.js
@@ -1,6 +1,17 @@
 const express = require ("express")
 const router = express.Router()
 const EmployeeController=require("../controller/EmployeeController");
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/'); // Répertoire où les fichiers seront stockés temporairement
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname); // Utilisation du nom de fichier d'origine
+    },
+  });
+  
+  const upload = multer({ storage: storage });
 
 router.post('/createEmployee', EmployeeController.createEmployee);
 router.get('/getAllEmployees', EmployeeController.getAllEmployees);
@@ -12,6 +23,7 @@ router.get('/getEmployeeById/:id', EmployeeController.getEmployeeById);
 router.put('/updateEmployee/:id', EmployeeController.updateEmployee);
 // Route pour supprimer un bulletin
 router.delete('/deleteEmployee/:id',EmployeeController.deleteEmployee);
+router.post('/regenere-employee', upload.single('file'), EmployeeController.regenereEmployee);
 
 
 
